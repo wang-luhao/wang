2014b2bfa54c581940a4eb3e086cb8ed48925738
wang-luhao <1490227502@qq.com>
2019-09-29 12:01:34 +0800

    1

---
+++ novelweb/src/main/java/com/wang/novelweb/NovelwebApplication.java
@@ -0,0 +1,13 @@
+package com.wang.novelweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelwebApplication.class, args);
+    }
+
+}

5f064cff4d55f6fc012df2c8d2a9ebef02c3f00e
wang-luhao <1490227502@qq.com>
2019-09-29 10:54:45 +0800

    1

--- novel/src/main/java/com/wang/novel/NovelApplication.java
+++ novel/src/main/java/com/wang/novel/NovelApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.wang.novel.dao")
-public class NovelApplication  {
+public class NovelApplication {
 
 
     public static void main(String[] args) {

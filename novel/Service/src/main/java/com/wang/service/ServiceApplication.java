4ac0612f348ede3d9819bf26fa47dcd0d686e690
wang-luhao <1490227502@qq.com>
2019-10-12 13:46:23 +0800

    1

--- novel/Service/src/main/java/com/wang/service/ServiceApplication.java
+++ novel/Service/src/main/java/com/wang/service/ServiceApplication.java
@@ -2,10 +2,12 @@ package com.wang.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@ConfigurationProperties()
 public class ServiceApplication {
 
     public static void main(String[] args) {

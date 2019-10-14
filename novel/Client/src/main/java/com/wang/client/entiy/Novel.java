d55e480dd2f8ffd5f08e62903170ed1b4ee4430f
wang-luhao <1490227502@qq.com>
2019-10-14 14:53:32 +0800

    1

---
+++ novel/Client/src/main/java/com/wang/client/entiy/Novel.java
@@ -0,0 +1,31 @@
+package com.wang.client.entiy;
+
+public class Novel {
+    private int novelId;
+    private String novelName;
+    private String novelAuthor;
+
+    public int getNovelId() {
+        return novelId;
+    }
+
+    public void setNovelId(int novelId) {
+        this.novelId = novelId;
+    }
+
+    public String getNovelName() {
+        return novelName;
+    }
+
+    public void setNovelName(String novelName) {
+        this.novelName = novelName;
+    }
+
+    public String getNovelAuthor() {
+        return novelAuthor;
+    }
+
+    public void setNovelAuthor(String novelAuthor) {
+        this.novelAuthor = novelAuthor;
+    }
+}

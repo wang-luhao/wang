0945d02edd647f53b8d0d36d3f44fde82a5916e4
wang-luhao <1490227502@qq.com>
2019-10-16 13:54:33 +0800

    1

--- novelweb/src/main/java/com/wang/novelweb/Controller/LoginController.java
+++ novelweb/src/main/java/com/wang/novelweb/Controller/LoginController.java
@@ -6,4 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+    public String Login(){
+        return "index";
+    }
 }

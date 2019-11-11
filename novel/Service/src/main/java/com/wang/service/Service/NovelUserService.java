7310ae2c807c2099463f6fd34af74e47374f6126
wang-luhao <1490227502@qq.com>
2019-11-11 11:05:27 +0800

    1

--- novel/Service/src/main/java/com/wang/service/Service/NovelUserService.java
+++
@@ -1,9 +0,0 @@
-package com.wang.service.Service;
-
-import com.wang.common.entity.novelUser;
-
-import java.util.List;
-
-public interface NovelUserService {
-    public List<novelUser> getUsers();
-}

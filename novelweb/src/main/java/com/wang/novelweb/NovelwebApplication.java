e29e4b8a99d23b95c19bbea4781e331b2bcca457
wang-luhao <1490227502@qq.com>
2019-11-12 14:42:59 +0800

    1

--- novelweb/src/main/java/com/wang/novelweb/NovelwebApplication.java
+++ novelweb/src/main/java/com/wang/novelweb/NovelwebApplication.java
@@ -3,6 +3,7 @@ package com.wang.novelweb;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@EnableSwagger2

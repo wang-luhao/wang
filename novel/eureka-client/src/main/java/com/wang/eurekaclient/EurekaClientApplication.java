a6e795b250e63b1d73d386339d53389c250d0764
wlh <1490227502@qq.com>
2020-01-09 17:17:33 +0800

    1

--- novel/eureka-client/src/main/java/com/wang/eurekaclient/EurekaClientApplication.java
+++ novel/eureka-client/src/main/java/com/wang/eurekaclient/EurekaClientApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
--- novel/eureka-server/src/main/java/com/wang/eurekaserver/EurekaServerApplication.java
+++ novel/eureka-server/src/main/java/com/wang/eurekaserver/EurekaServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {

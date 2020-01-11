4db8af518bc47235a9d42b6f29894ba3174945a6
wlh <1490227502@qq.com>
2020-01-11 14:19:58 +0800

    1

---
+++ novel/eureka-server/src/main/java/com/wang/eurekaserver/config/WebSecurityConfigure.java
@@ -0,0 +1,27 @@
+package com.wang.eurekaserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * author  wlh
+ * date  2020/1/10 0010 15:51
+ */
+@EnableWebSecurity
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 在/eureka/**端点忽略csrf验证
+        http.csrf().disable();
+//        // 配置使请求需要通过httpBasic或form验证
+//        http.authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .and()
+//                .httpBasic();
+        super.configure(http);
+    }
+}
